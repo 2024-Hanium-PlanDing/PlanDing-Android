1afeb488658b76e03b5e2fddff83179eff8c744a
comst19 <comst19@naver.com>
2024-04-13 17:04:55 +0900

    refact: BaseResponse 수정

--- PlanDing/data/src/main/java/com/comst/data/model/BaseResponse.kt
+++ PlanDing/data/src/main/java/com/comst/data/model/BaseResponse.kt
@@ -4,8 +4,22 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class BaseResponse<T>(
-    val success:String,
+    val success:Boolean,
     val data: T,
-    val errorResponse:T,
-    val status:String
+    val errorResponse:ErrorResponse,
+)
+
+@Serializable
+data class ErrorResponse(
+    val message: String,
+    val errorCode: Int,
+    val errors: List<FieldError>,
+    val status: String
+)
+
+@Serializable
+data class FieldError(
+    val field: String,
+    val value: String,
+    val reason: String
 )
