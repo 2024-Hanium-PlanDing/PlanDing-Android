e5de9403ef1162b199bc7b9247779f6a59b2c3b7
comst19 <comst19@naver.com>
2024-06-25 16:40:39 +0900

    refact: SingletonRepositoryModule, ViewModelRepositoryModule class -> object

--- PlanDing/data/src/main/java/com/comst/data/di/SingletonRepositoryModule.kt
+++ PlanDing/data/src/main/java/com/comst/data/di/SingletonRepositoryModule.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class SingletonRepositoryModule {
+object SingletonRepositoryModule {
 
     @Provides
     @Singleton
--- PlanDing/data/src/main/java/com/comst/data/di/ViewModelRepositoryModule.kt
+++ PlanDing/data/src/main/java/com/comst/data/di/ViewModelRepositoryModule.kt
@@ -17,7 +17,7 @@ import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
-class ViewModelRepositoryModule {
+object ViewModelRepositoryModule {
 
     @Provides
     @ViewModelScoped
