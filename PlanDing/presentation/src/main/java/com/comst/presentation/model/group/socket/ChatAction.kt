55b194f16110bbab05d244f80f30460bda8bc794
comst19 <comst19@naver.com>
2024-07-24 16:35:41 +0900

    feat: WebSocket 채팅 DTO

---
+++ PlanDing/presentation/src/main/java/com/comst/presentation/model/group/socket/ChatAction.kt
@@ -0,0 +1,7 @@
+package com.comst.presentation.model.group.socket
+
+enum class ChatAction(val description: String) {
+    JOIN("JOIN"),
+    CHAT("CHAT"),
+    LEAVE("LEAVE")
+}
--- PlanDing/presentation/src/main/java/com/comst/presentation/model/group/socket/WebSocketResponse.kt
+++ PlanDing/presentation/src/main/java/com/comst/presentation/model/group/socket/WebSocketResponse.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class WebSocketResponse<T>(
-    val success: Boolean,
+    val success: Boolean?,
     val data: T?,
     val errorResponse: ErrorResponse?
 )
