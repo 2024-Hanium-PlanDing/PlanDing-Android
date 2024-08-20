b4d888257dd2d5f41d09f477343fe803e5277889
comst19 <comst19@naver.com>
2024-08-20 22:01:14 +0900

    refact: CreateGroup 프리뷰 표시되도록 변경

--- PlanDing/presentation/src/main/java/com/comst/presentation/main/group/create/ImageSelectScreen.kt
+++ PlanDing/presentation/src/main/java/com/comst/presentation/main/group/create/ImageSelectScreen.kt
@@ -39,14 +39,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import com.comst.presentation.common.base.BaseScreen
-import com.comst.presentation.main.group.create.CreateGroupContract.CreateGroupIntent
-import com.comst.presentation.main.group.create.CreateGroupContract.CreateGroupSideEffect
+import com.comst.presentation.main.group.create.CreateGroupContract.*
+import com.comst.presentation.ui.theme.PlanDingTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ImageSelectScreen(
     viewModel: CreateGroupViewModel = hiltViewModel(),
@@ -64,103 +64,129 @@ fun ImageSelectScreen(
     }
 
     BaseScreen(viewModel = viewModel, handleEffect = handleEffect) { uiState ->
-        Surface {
-            Scaffold(
-                topBar = {
-                    CenterAlignedTopAppBar(
-                        title = {
-                            Text(
-                                text = "새 그룹",
-                                style = MaterialTheme.typography.headlineSmall
+        ImageSelectScreen(
+            uiState = uiState,
+            setIntent = viewModel::setIntent,
+            onBackClick = onBackClick,
+            onNextClick = onNextClick
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ImageSelectScreen(
+    uiState: CreateGroupUIState,
+    setIntent: (CreateGroupIntent) -> Unit = {},
+    onBackClick: () -> Unit = {},
+    onNextClick: () -> Unit = {}
+){
+    Surface {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(
+                            text = "새 그룹",
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = onBackClick) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = "뒤로가기"
                             )
-                        },
-                        navigationIcon = {
-                            IconButton(onClick = onBackClick) {
-                                Icon(
-                                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                    contentDescription = "뒤로가기"
-                                )
-                            }
-                        },
-                        actions = {
-                            TextButton(onClick = onNextClick) {
-                                Text(text = "다음", color = Color.Black)
-                            }
-                        },
-                        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary)
-                    )
-                },
-                content = { paddingValues ->
-                    Column(modifier = Modifier.padding(paddingValues)) {
-                        Box(
-                            modifier = Modifier.weight(1f),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            if (uiState.selectedImage != null) {
-                                Image(
-                                    modifier = Modifier.fillMaxSize(),
-                                    painter = rememberAsyncImagePainter(
-                                        model = uiState.selectedImage!!.uri
-                                    ),
-                                    contentScale = ContentScale.Crop,
-                                    contentDescription = null
-                                )
-                            } else {
-                                Text(
-                                    text = "선택된 이미지가 없습니다.",
-                                    modifier = Modifier.fillMaxWidth(),
-                                    textAlign = TextAlign.Center
-                                )
-                            }
                         }
-                        Divider(modifier = Modifier.height(1.dp))
-                        LazyVerticalGrid(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .weight(1f)
-                                .background(Color.White),
-                            columns = GridCells.Adaptive(110.dp),
-                            horizontalArrangement = Arrangement.spacedBy(2.dp),
-                            verticalArrangement = Arrangement.spacedBy(2.dp),
-                        ) {
-                            items(
-                                count = uiState.images.size,
-                                key = { index -> uiState.images[index].uri }
-                            ) { index ->
-                                val image = uiState.images[index]
-                                Box(
-                                    modifier = Modifier.clickable {
-                                        viewModel.setIntent(CreateGroupIntent.SelectGroupImage(image))
-                                    }
-                                ) {
-                                    Image(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .aspectRatio(1f),
-                                        painter = rememberAsyncImagePainter(
-                                            model = image.uri,
-                                            contentScale = ContentScale.Crop
-                                        ),
-                                        contentDescription = null,
+                    },
+                    actions = {
+                        TextButton(onClick = onNextClick) {
+                            Text(text = "다음", color = Color.Black)
+                        }
+                    },
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary)
+                )
+            },
+            content = { paddingValues ->
+                Column(modifier = Modifier.padding(paddingValues)) {
+                    Box(
+                        modifier = Modifier.weight(1f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        if (uiState.selectedImage != null) {
+                            Image(
+                                modifier = Modifier.fillMaxSize(),
+                                painter = rememberAsyncImagePainter(
+                                    model = uiState.selectedImage!!.uri
+                                ),
+                                contentScale = ContentScale.Crop,
+                                contentDescription = null
+                            )
+                        } else {
+                            Text(
+                                text = "선택된 이미지가 없습니다.",
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                    Divider(modifier = Modifier.height(1.dp))
+                    LazyVerticalGrid(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .background(Color.White),
+                        columns = GridCells.Adaptive(110.dp),
+                        horizontalArrangement = Arrangement.spacedBy(2.dp),
+                        verticalArrangement = Arrangement.spacedBy(2.dp),
+                    ) {
+                        items(
+                            count = uiState.images.size,
+                            key = { index -> uiState.images[index].uri }
+                        ) { index ->
+                            val image = uiState.images[index]
+                            Box(
+                                modifier = Modifier.clickable {
+                                    setIntent(CreateGroupIntent.SelectGroupImage(image))
+                                }
+                            ) {
+                                Image(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .aspectRatio(1f),
+                                    painter = rememberAsyncImagePainter(
+                                        model = image.uri,
                                         contentScale = ContentScale.Crop
+                                    ),
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Crop
+                                )
+                                if (uiState.selectedImage?.uri == image.uri) {
+                                    Icon(
+                                        modifier = Modifier
+                                            .padding(start = 4.dp, top = 4.dp)
+                                            .clip(CircleShape)
+                                            .background(color = Color.White),
+                                        imageVector = Icons.Filled.CheckCircle,
+                                        contentDescription = null,
+                                        tint = MaterialTheme.colorScheme.primary
                                     )
-                                    if (uiState.selectedImage?.uri == image.uri) {
-                                        Icon(
-                                            modifier = Modifier
-                                                .padding(start = 4.dp, top = 4.dp)
-                                                .clip(CircleShape)
-                                                .background(color = Color.White),
-                                            imageVector = Icons.Filled.CheckCircle,
-                                            contentDescription = null,
-                                            tint = MaterialTheme.colorScheme.primary
-                                        )
-                                    }
                                 }
                             }
                         }
                     }
-                },
-            )
-        }
+                }
+            },
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ImageSelectScreenPreview(){
+    PlanDingTheme {
+        ImageSelectScreen(
+            uiState = CreateGroupUIState()
+        )
     }
 }
