4828cac5d2edb365a01a599a739ea6520a350858
comst19 <comst19@naver.com>
2024-09-01 21:06:09 +0900

    feat: Api ErrorResponseInterceptor

---
+++ PlanDing/domain/src/main/java/com/comst/domain/util/CustomExceptions.kt
@@ -0,0 +1,8 @@
+package com.comst.domain.util
+
+import java.io.IOException
+
+class AccountNotFoundException(e: Throwable?, val url: String? = null) : IOException(e)
+class ServerNotFoundException(e: Throwable?, val url: String? = null) : IOException(e)
+class InternalServerErrorException(e: Throwable?, val url: String? = null) : IOException(e)
+class BadRequestException(e: Throwable?, val url: String? = null) : IOException(e)
