9a7d784ed528783520148cc6aed9c42d0712f8dd
comst19 <comst19@naver.com>
2024-09-20 18:50:16 +0900

    refact: 라이프사이클 상태에 따라 수집

--- PlanDing/presentation/src/main/java/com/comst/presentation/common/base/BaseScreen.kt
+++ PlanDing/presentation/src/main/java/com/comst/presentation/common/base/BaseScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
 import com.comst.presentation.auth.AuthActivity
+import com.comst.presentation.common.flow.collectAsStateWithLifecycle
+import com.comst.presentation.common.flow.collectWithLifecycle
 
 @Composable
 fun <S : UIState, A : BaseSideEffect, I : BaseIntent, E : BaseEvent> BaseScreen(
@@ -15,22 +17,21 @@ fun <S : UIState, A : BaseSideEffect, I : BaseIntent, E : BaseEvent> BaseScreen(
     handleEffect: (A) -> Unit = {},
     content: @Composable (S) -> Unit
 ) {
-    val uiState by viewModel.uiState.collectAsState()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     val context = LocalContext.current
 
-    LaunchedEffect(key1 = Unit) {
-        viewModel.effect.collect { effect ->
-            when (effect) {
-                is BaseSideEffect.ShowToast -> {
-                    Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show()
-                }
-                is BaseSideEffect.NavigateToLogin -> {
-                    context.startActivity(
-                        AuthActivity.authIntent(context)
-                    )
-                }
-                else -> handleEffect(effect)
+    viewModel.effect.collectWithLifecycle { effect ->
+        when (effect) {
+            is BaseSideEffect.ShowToast -> {
+                Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show()
             }
+            is BaseSideEffect.NavigateToLogin -> {
+                context.startActivity(
+                    AuthActivity.authIntent(context)
+                )
+            }
+            else -> handleEffect(effect)
         }
     }
 
--- PlanDing/presentation/src/main/java/com/comst/presentation/common/flow/Flow.kt
+++ PlanDing/presentation/src/main/java/com/comst/presentation/common/flow/Flow.kt
@@ -2,6 +2,7 @@ package com.comst.presentation.common.flow
 
 import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -17,6 +18,7 @@ import kotlin.coroutines.EmptyCoroutineContext
 
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
+//StateFlow로 관리되는 상태(State)를 View에서 구독할 때 사용
 fun <T> StateFlow<T>.collectAsStateWithLifecycle(
     lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
@@ -28,6 +30,7 @@ fun <T> StateFlow<T>.collectAsStateWithLifecycle(
     context = context
 )
 
+//StateFlow가 아닌 일반적인 Flow를 상태 관리용으로 사용 => 서버에서 데이터를 가져오거나 데이터 스트림을 처리
 @Composable
 internal fun <T> Flow<T>.collectAsStateWithLifecycle(
     initialValue: T,
@@ -42,6 +45,21 @@ internal fun <T> Flow<T>.collectAsStateWithLifecycle(
             } else withContext(context) {
                 this@collectAsStateWithLifecycle.collect { this@produceState.value = it }
             }
+        }
+    }
+}
+
+// 단발성 이벤트를 처리할 때 사용
+@Composable
+fun <T> Flow<T>.collectWithLifecycle(
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    action: suspend (T) -> Unit, // reified 제거
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(this, lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
+            this@collectWithLifecycle.collect { action(it) }
         }
     }
 }
